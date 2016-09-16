3730d102b2f2cc2679f63cbfedebe591c75fb426
Anton Lehmus <anton.lehmus@gmail.com>
2016-09-16 16:45:28 +0300

    add utf-8 encoding tag to header

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -11,19 +11,15 @@ use Illuminate\Http\Request;
 class ScoreController extends Controller{
 
    public function index(){
+		$headers = [ 'Content-Type' => 'application/json; charset=utf-8' ];
 		 $scores  = Score::orderBy('score','desc')->get();
-		 return response()->json($scores);
+		 return response()->json($scores, 200, $headers, JSON_UNESCAPED_UNICODE);
    }
 
 	 public function limit($limit){
-		 if(isset($limit) && $limit != null)
-		 {
-			 $scores  = Score::orderBy('score','desc')->take(intval($limit))->get();
-		 }
-		 else {
-			 $scores  = Score::orderBy('score','desc')->get();
-		 }
-		 return response()->json($scores);
+		$headers = [ 'Content-Type' => 'application/json; charset=utf-8' ];
+		$scores  = Score::orderBy('score','desc')->take(intval($limit))->get();
+		return response()->json($scores, 200, $headers, JSON_UNESCAPED_UNICODE);
    }
 
    public function createScore(Request $request){
